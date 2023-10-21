25654c37276fdfebe2f4ce149c6a7b26ad69e1da
Shuai An <413959124@qq.com>
2023-10-21 19:32:45 +0100

    update files

--- utils.py
+++ utils.py
@@ -3,11 +3,13 @@
 import yaml
 import csv
 import os
+import random
+import numpy as np
 
 def check_exp(data_df, beexplain_id, final_subsets):
     beexplain_value = data_df.loc[beexplain_id]
     df = data_df.copy()
-
+    
     for key in final_subsets:
         #  print(key)
         df = df.loc[(df[key] == beexplain_value[key])]
@@ -22,7 +24,6 @@ def alg_config_parse(filename='config.yaml'):
             return alg_dict
         except yaml.YAMLError as exc:
             print(exc)
-
 
 def set_distance(list1, list2):
     return len(set(list1) ^ set(list2))  
@@ -44,6 +45,7 @@ def select_features(feature_scores, level=0.8):
 
 
 def compute_con_acc(df, instance_value, feature_list):
+    # specified_row = df.loc[instance_id]
     specified_row = instance_value
 
     mask_same_features = (df[feature_list] == specified_row[feature_list]).all(axis=1)
@@ -67,3 +69,89 @@ def compute_recall(df, instance_value, feature_list1, feature_list2):
     mask_same_features = mask_same_features1 | mask_same_features2
     
     return round(mask_same_features1.sum() / mask_same_features.sum(), 3)
+
+
+
+def compute_faithfulness_er(test_df, instance_value, tem_exp, predict_fn):
+
+    new_instance_value = instance_value.copy()
+    
+    for col in tem_exp:
+        possible_replacements = test_df[test_df[col] != instance_value[col]][col].unique()
+        
+        if len(possible_replacements) == 0:
+            print(f"No alternative values available for column {col}. Skipping...")
+            continue
+        
+        new_value = random.choice(possible_replacements)
+        new_instance_value[col] = new_value
+    
+    rest_values = new_instance_value.iloc[:-1]
+    mid_idx = len(rest_values) // 2  
+    l_tuple = rest_values[:mid_idx]
+    r_tuple = rest_values[mid_idx:]
+    from certamain.certa.local_explain import get_original_prediction
+    prediction = get_original_prediction(l_tuple, r_tuple, predict_fn)
+    pre_class = np.argmax(prediction)
+    
+    if new_instance_value.iloc[-1] == pre_class:
+        return True
+    else:
+        return False
+
+
+def compute_faithfulness_er_fnum(test_df, instance_value, sorted_column_names, predict_fn): 
+    
+    same_res_num = 0
+
+    tem_exp = sorted_column_names
+    new_instance_value = instance_value.copy()
+    
+    for col in tem_exp:
+        possible_replacements = test_df[test_df[col] != instance_value[col]][col].unique()
+        if len(possible_replacements) == 0:
+            print(f"No alternative values available for column {col}. Skipping...")
+            continue
+        
+        new_value = random.choice(possible_replacements)            
+        new_instance_value[col] = new_value
+        
+    rest_values = new_instance_value.iloc[:-1]
+    mid_idx = len(rest_values) // 2  
+    l_tuple = rest_values[:mid_idx]
+    r_tuple = rest_values[mid_idx:]
+    from certamain.certa.local_explain import get_original_prediction
+    prediction = get_original_prediction(l_tuple, r_tuple, predict_fn)
+    pre_class = np.argmax(prediction)
+
+    if new_instance_value.iloc[-1] == pre_class:
+         same_res_num += 1
+    
+    return same_res_num/1
+
+
+def compute_faithfulness(test_df, instance_value, sorted_name_exp, best_model): 
+            
+    same_res_num = 0
+    tem_exp = sorted_name_exp
+    new_instance_value = instance_value.copy()
+    
+    for col in tem_exp:
+        
+        possible_replacements = test_df[test_df[col] != instance_value[col]][col].unique()
+        
+        if len(possible_replacements) == 0:
+            print(f"No alternative values available for column {col}. Skipping...")
+            continue
+        
+        new_value = random.choice(possible_replacements)            
+        new_instance_value[col] = new_value
+        
+    prediction = best_model.predict(new_instance_value.iloc[0:-1].values.reshape(1, -1))[0]
+
+    if new_instance_value.iloc[-1] == prediction:
+         same_res_num += 1
+    
+    return same_res_num/1  
+
+
